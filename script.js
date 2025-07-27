4a25bd89f9a9b9f6cf585a4be92fc2444fe21a9f
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-07-27 15:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function value(n){
 	else if(n%3==0){
 		retun 'Fizz'
 	}
-	else if((n%5==0 && n%3==0) return 'FizzBuzz'
-	else n;
+	else if((n%5==0 && n%3==0) {
+		return 'FizzBuzz'
+ }
+	else {
+		return n;
+	}
 }
